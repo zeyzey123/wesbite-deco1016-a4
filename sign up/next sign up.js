03d34e5de60a199ebfa8a4105fef4b928faafc0b
zeyzey123 <57380801+zeyzey123@users.noreply.github.com>
2019-11-05 14:06:23 +1100

    intial

---
+++ sign up/next sign up.js
@@ -0,0 +1,13 @@
+let overEighteen = isOverEighteen(age);
+
+function isOverEighteen(age) {    // Function requires a variable to be passed in as an input parameter
+    if (age >= 18) {                // Comparison operator checks value of input parameter
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  function updatePage(){
+      
+  }
